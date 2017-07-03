1011ea3813ca9203d761aecbdefef47721f99659
Tiep Le <lionking1109@live.com>
2017-07-03 08:49:46 +0700

    Update

--- Server/InternalClient.cs
+++ Server/InternalClient.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Text;
 using Core;
-using Helios.Net;
-using Helios.Topology;
 
 namespace Server
 {
@@ -10,66 +8,65 @@ namespace Server
     {
         private readonly Guid _guid;
         private readonly ILog _logger;
-        private INode _address;
-        private IConnection _tcpConnection;
+
 
         public event ClientDisconnectedCallBack OnDisconnect;
 
-        public InternalClient(Guid guid, ILog logger, INode address, IConnection tcpConnection)
-        {
-            _guid = guid;
-            _logger = logger;
-            _address = address;
-            _tcpConnection = tcpConnection;
+        //public InternalClient(Guid guid, ILog logger, INode address, IConnection tcpConnection)
+        //{
+        //    _guid = guid;
+        //    _logger = logger;
+        //    _address = address;
+        //    _tcpConnection = tcpConnection;
 
-            _tcpConnection.OnDisconnection += OnTcpDisconnect;
-            _tcpConnection.BeginReceive(ReceiveTCP);
-        }
+        //    _tcpConnection.OnDisconnection += OnTcpDisconnect;
+        //    _tcpConnection.BeginReceive(ReceiveTCP);
+        //}
 
-        /// <summary>
-        /// Khi client bị disconnect vì bất cứ lí do gì thì đây là event để handle
-        /// </summary>
-        /// <param name="reason"></param>
-        /// <param name="address"></param>
-        private void OnTcpDisconnect(Helios.Exceptions.HeliosConnectionException reason, IConnection address)
-        {
-            Console.WriteLine("Disconnected: {0}; Reason: {1}", address.RemoteHost, reason.Type);
-            _logger.Info($"Disconnected: {address.RemoteHost}; Reason: {reason.Type}");
+        ///// <summary>
+        ///// Khi client bị disconnect vì bất cứ lí do gì thì đây là event để handle
+        ///// </summary>
+        ///// <param name="reason"></param>
+        ///// <param name="address"></param>
+        //private void OnTcpDisconnect(Helios.Exceptions.HeliosConnectionException reason, IConnection address)
+        //{
+        //    Console.WriteLine("Disconnected: {0}; Reason: {1}", address.RemoteHost, reason.Type);
+        //    _logger.Info($"Disconnected: {address.RemoteHost}; Reason: {reason.Type}");
             
-            // Cleanup other connection here, then notify Disconnect to Server
+        //    // Cleanup other connection here, then notify Disconnect to Server
 
-            NotifyDisconnect();
-        }
+        //    NotifyDisconnect();
+        //}
 
-        /// <summary>
-        /// Nhận dữ liệu qua TCP
-        /// </summary>
-        /// <param name="data"></param>
-        /// <param name="channel"></param>
-        public static void ReceiveTCP(NetworkData data, IConnection channel)
-        {
-            var command = Encoding.UTF8.GetString(data.Buffer);
+        ///// <summary>
+        ///// Nhận dữ liệu qua TCP
+        ///// </summary>
+        ///// <param name="data"></param>
+        ///// <param name="channel"></param>
+        //public static void ReceiveTCP(NetworkData data, IConnection channel)
+        //{
+        //    var command = Encoding.UTF8.GetString(data.Buffer);
 
-            //Console.WriteLine("Received: {0}", command);
-            if (command.ToLowerInvariant() == "gettime")
-            {
-                var time = Encoding.UTF8.GetBytes(DateTime.Now.ToLongTimeString());
-                channel.Send(new NetworkData { Buffer = time, Length = time.Length, RemoteHost = channel.RemoteHost });
-                //Console.WriteLine("Sent time to {0}", channel.Node);
-            }
-            else
-            {
-                Console.WriteLine("Invalid command: {0}", command);
-                var invalid = Encoding.UTF8.GetBytes("Unrecognized command");
+        //    //Console.WriteLine("Received: {0}", command);
+        //    if (command.ToLowerInvariant() == "gettime")
+        //    {
+        //        var time = Encoding.UTF8.GetBytes(DateTime.Now.ToLongTimeString());
+        //        channel.Send(new NetworkData { Buffer = time, Length = time.Length, RemoteHost = channel.RemoteHost });
+        //        //Console.WriteLine("Sent time to {0}", channel.Node);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Invalid command: {0}", command);
+        //        var invalid = Encoding.UTF8.GetBytes("Unrecognized command");
 
-                channel.Send(new NetworkData
-                {
-                    Buffer = invalid,
-                    Length = invalid.Length,
-                    RemoteHost = channel.RemoteHost
-                });
-            }
-        }
+        //        channel.Send(new NetworkData
+        //        {
+        //            Buffer = invalid,
+        //            Length = invalid.Length,
+        //            RemoteHost = channel.RemoteHost
+        //        });
+        //    }
+        //}
 
         /// <summary>
         /// Thông báo cho server biết Client này bị disconnect
--- Server/Server.cs
+++ Server/Server.cs
@@ -4,29 +4,18 @@ using System.Net;
 using System.Text;
 using System.Threading;
 using Core;
-using Helios.Channels;
-using Helios.Channels.Bootstrap;
-using Helios.Channels.Sockets;
-using Helios.Net;
-using Helios.Ops.Executors;
-using Helios.Reactor;
-using Helios.Topology;
 
 namespace Server
 {
     public class Server : IServer
     {
         private readonly ILog _logger;
-        IReactor _server;
+        //IReactor _server;
 
         public IPAddress Host { get; }
         public int Port { get; }
 
         private Dictionary<Guid, IInternalClient> _clients;
-        private CancellationTokenSource _shutdownToken;
-
-        public MultithreadEventLoopGroup WorkerGroup { get; set; }
-        public MultithreadEventLoopGroup ServerGroup { get; set; }
 
         public Server(ILog logger, IPAddress ipAddress, int port)
         {
@@ -36,48 +25,45 @@ namespace Server
 
             _clients = new Dictionary<Guid, IInternalClient>();
 
-            ServerGroup = new MultithreadEventLoopGroup(1);
-            WorkerGroup = new MultithreadEventLoopGroup(Environment.ProcessorCount / 2);
 
-            _shutdownToken = new CancellationTokenSource();
         }
 
         public void Start()
         {
-            var executor = new TryCatchExecutor(exception => Console.WriteLine("Unhandled exception: {0}", exception));
+            //var executor = new TryCatchExecutor(exception => Console.WriteLine("Unhandled exception: {0}", exception));
 
-            var bootstrapper = new ServerBootstrap()
-                .Group(ServerGroup, WorkerGroup)
-                .Channel<TcpServerSocketChannel>()
-                .ChildOption(ChannelOption.TcpNodelay, true)
-                .ChildHandler(new ActionChannelInitializer<TcpSocketChannel>(channel =>
-                {
-                    channel.Pipeline.AddLast(GetEncoder())
-                        .AddLast(GetDecoder())
-                        .AddLast(new IntCodec(true))
-                        .AddLast(new CounterHandlerInbound(_inboundThroughputCounter))
-                        .AddLast(new CounterHandlerOutbound(_outboundThroughputCounter))
-                        .AddLast(new ErrorCounterHandler(_errorCounter));
-                }));
+            //var bootstrapper = new ServerBootstrap()
+            //    .Group(ServerGroup, WorkerGroup)
+            //    .Channel<TcpServerSocketChannel>()
+            //    .ChildOption(ChannelOption.TcpNodelay, true)
+            //    .ChildHandler(new ActionChannelInitializer<TcpSocketChannel>(channel =>
+            //    {
+            //        channel.Pipeline.AddLast(GetEncoder())
+            //            .AddLast(GetDecoder())
+            //            .AddLast(new IntCodec(true))
+            //            .AddLast(new CounterHandlerInbound(_inboundThroughputCounter))
+            //            .AddLast(new CounterHandlerOutbound(_outboundThroughputCounter))
+            //            .AddLast(new ErrorCounterHandler(_errorCounter));
+            //    }));
 
-            _server = bootstrapper.NewReactor(NodeBuilder.BuildNode().Host(Host).WithPort(Port));
-            _server.OnConnection += OnTcpConnection;
+            //_server = bootstrapper.NewReactor(NodeBuilder.BuildNode().Host(Host).WithPort(Port));
+            //_server.OnConnection += OnTcpConnection;
 
-            _server.Start();
+            //_server.Start();
 
             Console.WriteLine("Running...");
         }
                 
-        private void OnTcpConnection(INode address, IConnection connection)
-        {
-            Console.WriteLine("Connected: {0}", address);
+        //private void OnTcpConnection(INode address, IConnection connection)
+        //{
+        //    Console.WriteLine("Connected: {0}", address);
 
-            var guid = Guid.NewGuid();
-            IInternalClient internalClient = new InternalClient(guid, _logger, address, connection);
-            internalClient.OnDisconnect += OnClientDisconnected;
+        //    var guid = Guid.NewGuid();
+        //    IInternalClient internalClient = new InternalClient(guid, _logger, address, connection);
+        //    internalClient.OnDisconnect += OnClientDisconnected;
 
-            _clients.Add(guid, internalClient);
-        }
+        //    _clients.Add(guid, internalClient);
+        //}
 
         private void OnClientDisconnected(Guid clientGuid)
         {
@@ -89,12 +75,12 @@ namespace Server
 
         public void Stop()
         {
-            _shutdownToken.Cancel();
+            //_shutdownToken.Cancel();
 
             _clients.Clear();
 
             Console.WriteLine("Shutting down...");
-            _server.Stop();
+            //_server.Stop();
             Console.WriteLine("Terminated");
         }
 
